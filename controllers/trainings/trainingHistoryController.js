72b93ccdb794fbe461bbb0816d1aab4140844964
dranchukroma <roman2003dranchuk@gmail.com>
2025-05-03 16:15:55 +0200

    Changes for description in WUM

---
+++ controllers/trainings/trainingHistoryController.js
@@ -0,0 +1,110 @@
+import TrainignHistory from "../../models/trainings/trainingHistory";
+
+export async function getAllTrainingRecords(req, res) {
+    const { id } = req.user;
+    try {
+        if(!id) return res.status(400).json({ message: 'Missed require fields'});
+
+        const records = await TrainignHistory.getAllRecordsFromTrainingHistory(id);
+
+        return res.status(200).json({
+            message: 'Exercise records fetched successfully',
+            data: records || []
+        });
+    } catch (error) {
+        console.error(`Getting all training record for ${id} failed: `, error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function getHistoryRecordByExercise(req, res){
+    const { id } = req.user;
+    try {
+        const { exercise_id } = req.body;
+        if(!id || !exercise_id) return res.status(400).json({ message: 'Missed require fields' });
+
+        const result = false;
+
+        return res.status(200).json({
+            message: 'Exercise records fetched successfully',
+            data: result || []
+        });
+    } catch (error) {
+        console.error(`Getting all training record for ${id} failed: `, error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function addTrainingRecord(req, res) {
+    const { id } = req.user;
+    try {
+        const { plan_id, 
+            day_id, 
+            exercise_id, 
+            date, 
+            completed, 
+            sets_completed, 
+            reps_completed, 
+            weight_used, 
+            notes } = req.body;
+        if(!id || 
+            !plan_id || 
+            !day_id || 
+            !exercise_id || 
+            !date || 
+            !completed || 
+            !sets_completed || 
+            !reps_completed || 
+            !weight_used || 
+            !notes) return res.status(400).json({ message: 'Missed require fields'});
+
+        const result = await TrainignHistory.addRecordToTrainingHistory(id, plan_id, day_id, exercise_id, date, completed, sets_completed, reps_completed, weight_used, notes);
+        if(!result) return res.status(401).json({ message: 'Training record has not been added' });
+        
+        return res.status(201).json({ message: 'Training record has been added'});
+    } catch (error) {
+        console.error(`Adding training record for ${id} failed: `, error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function updateTrainingRecord(req, res) {
+    const { id } = req.user;
+    try {
+        const { history_id, 
+            sets_completed, 
+            reps_completed, 
+            weight_used, 
+            notes } = req.body;
+        if(!id ||
+            !history_id ||
+            !sets_completed || 
+            !reps_completed || 
+            !weight_used || 
+            !notes) return res.status(400).json({ message: 'Missed require fields'});
+
+        const result = await TrainignHistory.addRecordToTrainingHistory(history_id, sets_completed, reps_completed, weight_used, notes);
+        if(!result) return res.status(401).json({ message: 'Training record has not been updated' });
+        
+        return res.status(201).json({ message: 'Training record has been added'});
+    } catch (error) {
+        console.error(`Updating training record for ${id} failed: `, error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function deleteTrainingRecord(req, res) {
+    const { id } = req.user
+    try {
+        const { history_id } = req.body;
+        if(!id || !history_id) return res.status(400).json({ message: 'Missed require fields'});
+
+        const result = await TrainignHistory.deleteRecordFromTrainingHistory(history_id);
+        if(!result) return res.status(404).json({ message: `Training record with id ${history_id} has not been deleted`} );
+
+        return res.status(200).json({ message: `Training record with id ${history_id} has been deleted` })
+    } catch (error) {
+        console.error(`Deleting training record for ${id} failed: `, error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
--- models/trainings/trainingHistory.js
+++ models/trainings/trainingHistory.js
@@ -7,11 +7,22 @@ class TrainignHistory {
     }
   }
 
-  static  async addRecordToTrainingHistory(user_id, plan_id, day_id, exercise_id, date, completed, sets_completed, reps_completed, weight_used, notes){
+  static async addRecordToTrainingHistory(user_id, plan_id, day_id, exercise_id, date, completed, sets_completed, reps_completed, weight_used, notes){
     try {
         const result = await db.query(`
-            INSERT INTO training_history (user_id, plan_id, day_id, exercise_id, date, completed, sets_completed, reps_completed, weight_used, notes) 
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+            INSERT INTO training_history (
+                user_id, 
+                plan_id, 
+                day_id, 
+                exercise_id, 
+                date, 
+                completed, 
+                sets_completed, 
+                reps_completed, 
+                weight_used, 
+                notes
+            ) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
         `, [
             user_id, 
             plan_id, 
@@ -25,29 +36,46 @@ class TrainignHistory {
             notes
         ])
 
-        return true;
+        return result.rowCount > 0;
     } catch (error) {
-        console.log('Error while adding record to training history: ', error);
+        console.error(`Adding training record to DB for ${user_id} failed: `, error);
         return false;
     }
   }
 
-  static  async getAllRecordsFromTrainingHistory(user_id){
+  static async getAllRecordsFromTrainingHistory(user_id){
     try {
         const result = await db.query(`
-            SELECT * FROM training_history WHERE user_id = ? ORDER BY date DESC;
+            SELECT * FROM training_history 
+            WHERE user_id = $1 
+            ORDER BY date DESC;
         `, [
             user_id
         ])
 
         return result.rows;
     } catch (error) {
-        console.log('Error while getting all records from training history: ', error);
+        console.error(`Getting all training records from DB for ${user_id} failed: `, error);
         return false;
     }
   }
 
-  static  async updateRecordInTrainingHistory(history_id, sets_completed, reps_completed, weight_used, notes){
+  static async getRecordByExercise(user_id, exercise_id){
+    try {
+        const result = await db.query(`
+            SELECT * FROM training_history
+            WHERE user_id = $1
+            AND exercise_id = $2    
+        `, [ user_id, exercise_id ]);
+
+        return result.rows;
+    } catch (error) {
+        console.error(`Getting exercise records for ${user_id} failed: `, error);
+        return false;
+    }
+  }
+
+  static async updateRecordInTrainingHistory(history_id, sets_completed, reps_completed, weight_used, notes){
     try {
         const result = await db.query(`
             UPDATE training_history 
@@ -61,14 +89,14 @@ class TrainignHistory {
             notes
         ])
 
-        return true;
+        return result.rowCount > 0;
     } catch (error) {
-        console.log('Error while updating record in training history: ', error);
+        console.error(`Updating training record with id ${history_id} in DB failed: `, error);
         return false;
     }
   }
 
-  static  async deleteRecordFromTrainingHistory(history_id){
+  static async deleteRecordFromTrainingHistory(history_id){
     try {
         const result = await db.query(`
             DELETE FROM training_history WHERE history_id = $1;
@@ -76,9 +104,9 @@ class TrainignHistory {
             history_id
         ])
 
-        return true;
+        return result.rowCount > 0;
     } catch (error) {
-        console.log('Error while deleting record from training history: ', error);
+        console.error(`Deleting training record with id ${history_id} from DB failed: `, error);
         return false;
     }
   }
